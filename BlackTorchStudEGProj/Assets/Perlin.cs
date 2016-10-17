6ad7c116cf487f7762a5b7e8729fb143dc821b29
Briken <brokenbriken@gmail.com>
2016-10-17 15:16:49 +0100

    PerlinNoise Implemented
    perlin noise implemented with varying colour on the terrain, dirt and
    grass, mountain implementation to follow

---
+++ BlackTorchStudEGProj/Assets/Perlin.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Perlin  {
+
+    long seed;
+
+    public Perlin(long seed)
+    {
+        this.seed = seed;
+    }
+
+    private int random(int x, int range)
+    {
+        return (int)((x+seed)^5)%range;
+    }
+
+    public int GetNoise(int x, int range)
+    {
+        int chunkSize = 16;
+        int chunkIndex = x / chunkSize;
+
+        float noise = 0;
+
+        range /= 2;
+
+        while (chunkSize > 0)
+        {
+            float distFromIndex = (x % chunkSize) / (chunkSize * 1.0f);
+
+            float lRandom = random(chunkIndex, range);
+            float rRandom = random(chunkIndex + 1, range);
+            noise += (1 - distFromIndex) * lRandom + distFromIndex * rRandom;
+
+            chunkSize /= 2;
+            range /= 2;
+
+            range = Mathf.Max(1, range);
+        }
+        return (int)Mathf.Round(noise);
+    }
+}
---
+++ BlackTorchStudEGProj/Assets/TerrainGen.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainGen : MonoBehaviour {
+
+    public GameObject[,] terrainArray;
+    GameObject currentBlock;
+
+    public GameObject dirt;
+    public GameObject rock;
+    public GameObject grass;
+    public GameObject mineral;
+
+    int minX = -16;
+    int maxX = 16;
+    int minY = -16;
+    int maxY = 16;
+
+    Perlin perlinNoise;
+
+	// Use this for initialization
+	void Start ()
+    {
+        perlinNoise = new Perlin(Random.Range(1000000, 100000000));
+        GridGen();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    private void GridGen()
+    {
+        float width = dirt.transform.localScale.x;
+        float height = dirt.transform.localScale.y;
+
+        for (int x = minX; x < maxX; x++)
+        {
+            int colHeight = 2 + perlinNoise.GetNoise(x - minX, maxY - minY - 2);
+            for (int y = minY; y < minY + colHeight; y++)
+            {
+                GameObject block = dirt;
+                if ((y == minY + colHeight - 1))
+                {
+                    block = grass;
+                }
+                if (y == minY + colHeight)
+                {
+                    block = rock;
+                }
+                
+                Instantiate(block, new Vector2(x /** width*/, y /** height*/), Quaternion.identity);
+
+            }
+        }
+    }
+}
